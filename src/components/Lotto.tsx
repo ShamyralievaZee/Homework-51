1836f9fad5d1dfeef70eb22bfacef768a1d74c60
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-09-12 15:02:57 +0600

    Updated & fixed Lotto component

---
+++ src/components/Lotto.tsx
@@ -0,0 +1,8 @@
+import './Lotto.css';
+import React from "react";
+
+const Lotto: React.FC<{ number: number }> = ({ number }) => (
+    <div className="lotto-ball">{number}</div>
+);
+
+export default Lotto;
