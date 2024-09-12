01936484e58d76f6d1e182436f1f4284188a9811
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-09-12 15:08:43 +0600

    Worked on generateRandomNumbers function for the numbers to be counted, generated, sorted and returned in the needed state. Worked on App function, so the initial numbers will be shown first and new numbers will be handled once the button is pressed. And made sure the displaying is correct.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import './App.css';
+import './components/Lotto.tsx';
+import React, { useState } from 'react';
+import Lotto from './components/Lotto.tsx';
 
-const App = () => {
-  return (
-    <>
-Hello world
-    </>
-  );
+const generateRandomNumbers = () => {
+    const numbers = new Set<number>();
+    while (numbers.size < 5) {
+        const randomNumber = Math.floor(Math.random() * 32) + 5;
+        numbers.add(randomNumber);
+    }
+    return Array.from(numbers).sort((a, b) => a - b);
+};
+
+const App: React.FC = () => {
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const handleNewNumbers = () => {
+        setNumbers(generateRandomNumbers());
+    };
+
+    return (
+        <div className="app">
+            <button type='button' onClick={handleNewNumbers}>New numbers</button>
+            <div className="lotto-balls">
+                {numbers.map(number => (
+                    <Lotto key={number} number={number}/>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default App;
