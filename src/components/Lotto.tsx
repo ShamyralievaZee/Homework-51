779c54132de0fbac7669e8774c28239e053dbc1c
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-09-12 15:50:39 +0600

    fixed the issue with the semicolon

--- src/components/Lotto.tsx
+++ src/components/Lotto.tsx
@@ -1,5 +1,5 @@
 import './Lotto.css';
-import React from "react";
+import React from 'react';
 
 const Lotto: React.FC<{ number: number }> = ({ number }) => (
     <div className="lotto-ball">{number}</div>
